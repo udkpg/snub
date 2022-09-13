84f0e21330744f32d97bce33cc74bb6ef0613aa3
Obstronomic <113385643+udkpg@users.noreply.github.com>
2022-09-13 21:28:28 +0200

    loggy

--- js/index.js
+++ js/index.js
@@ -1,180 +1,89 @@
-// sorry for the spaghetti code and redundant variables, i wasn't exactly a good coder back then
+function rain(el=document.body, params) {
+	if (!el){
+		console.error('Must have element to populate the rain!');
+		return;
+	}
+	const defaultProperties = {
+		colors: 'default',
+		drop: window.innerHeight,
+		fadeout: true,
+		raindrops: 400,
+		speed: 2000,
+		splashes: true,
+		spread: window.innerWidth
+	};
 
-const cols = 3;
-const main = document.getElementById('main');
-let parts = [];
-
-let images = [
-  "https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
-  "https://images.unsplash.com/photo-1544198365-f5d60b6d8190?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
-  "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80"
-];
-let current = 0;
-let playing = false;
-
-for (let i in images) {
-  new Image().src = images[i];
+	// properties passed in from user onto the defaults
+	const c = Object.assign(defaultProperties, params);
+	
+	const randInt = (min, max) => {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	};
+	let hh = window.innerHeight;
+	let ww = window.innerWidth;
+	let raindrops = '';
+	let rainsplashes = '';
+	if (!c.flakes || Number.isNaN(c.flakes * 1)){
+		c.flakes = 250;
+	}
+	for (let i = 0; i < c.flakes; i++) {
+		let dur = c.speed;
+		raindrops += `<g transform="translate(${randInt(0,ww)} -26) scale(1.${randInt(0, 3)})">
+		<rect id="snowflake${i}" fill="rgba(240, 240, 255, ${randInt(1,10)/10})" x="0" y="0" rx="1" height="${randInt(4,10)}" width="${randInt(3,5)/10}" filter="url(#blur${randInt(1, 2)})">
+		</rect>
+	</g>
+	<animateMotion xlink:href="#snowflake${i}" dur="${dur}ms" begin="-${randInt(0,100)/10}s" repeatCount="indefinite">
+		<mpath xlink:href="#motionPath${randInt(1, 2)}" />
+	</animateMotion>`;
+		// animated circle splashes
+		if (c.splashes) {
+			if (randInt(0,1) == 1) {
+			let randTiming = -randInt(0, 10) / 10;
+			rainsplashes += `<ellipse stroke="rgba(240, 240, 255, ${randInt(1,4)/10})" stroke-width="1" fill="none" cx="${randInt(0, ww)}" cy="${randInt(hh, hh/1.2)}" rx="5" ry="3">
+			<animate
+				 attributeType="XML" attributeName="stroke-width"
+				 dur="1s" values="0; 0.7; 0"
+				 repeatCount="indefinite" begin="${randTiming}" />
+			<animate
+				 attributeType="XML" attributeName="rx"
+				 dur="${c.speed / 2}ms" values="0; ${randInt(5,9)}"
+				 repeatCount="indefinite" additive="sum" begin="${randTiming}" />
+			<animate
+				 attributeType="XML" attributeName="ry"
+				 dur="${c.speed / 2}ms" values="0; ${randInt(1,2)}"
+				 repeatCount="indefinite" additive="sum" begin="${randTiming}" />
+				 <!--
+			<animate
+				 attributeType="XML" attributeName="cx"
+				 dur="${c.speed / 2}ms" values="${randInt(0,ww)}; ${randInt(0,ww)}"
+				 repeatCount="indefinite" additive="sum" begin="${randTiming}" />
+				 -->
+			</ellipse>`;
+			}
+		}
+	}
+	
+	let svg = `<svg id="snowverlay" viewbox="0 0 ${ww} ${hh}" height="${hh}" width="${ww}" preserveAspectRatio="none" style="z-index:99999; user-select:none; pointer-events:none; top:50%;
+	left:50%; position:fixed; transform:translate(-50%,-50%)">
+	<filter id="blur1" x="-100%" y="-100%" width="300%" height="300%">
+		<feGaussianBlur in="SourceGraphic" stdDeviation="1" />
+	</filter>
+	<filter id="blur2" x="-100%" y="-100%" width="300%" height="300%">
+		<feGaussianBlur in="SourceGraphic" stdDeviation="2" />
+	</filter>
+	<path id="motionPath1" fill="none" stroke="none" d="M 0 -${hh * 0.1} V ${hh * 1.1}" />
+	<path id="motionPath2" fill="none" stroke="none" d="M 0 -${hh * 0.1} V ${hh * 1.1}" />
+	${raindrops}
+	${rainsplashes}
+</svg>`;
+	//Make it a node to avoid the dangerous "document.body.innerHTML = svg"
+	let wrapper = document.createElement("div");
+	wrapper.innerHTML = svg;
+	let doc = wrapper.firstChild;
+	const element = document.getElementById("snowverlay");
+	element?.remove();
+	document.body.appendChild(doc);
 }
 
-for (let col = 0; col < cols; col++) {
-  let part = document.createElement('div');
-  part.className = 'part';
-  let el = document.createElement('div');
-  el.className = "section";
-  let img = document.createElement('img');
-  img.src = images[current];
-  el.appendChild(img);
-  part.style.setProperty('--x', -100/cols*col+'vw');
-  part.appendChild(el);
-  main.appendChild(part);
-  parts.push(part);
-}
-
-let animOptions = {
-  duration: 2.3,
-  ease: Power4.easeInOut
-};
-
-function go(dir) {
-  if (!playing) {
-    playing = true;
-    if (current + dir < 0) current = images.length - 1;
-    else if (current + dir >= images.length) current = 0;
-    else current += dir;
-
-    function up(part, next) {
-      part.appendChild(next);
-      gsap.to(part, {...animOptions, y: -window.innerHeight}).then(function () {
-        part.children[0].remove();
-        gsap.to(part, {duration: 0, y: 0});
-      })
-    }
-
-    function down(part, next) {
-      part.prepend(next);
-      gsap.to(part, {duration: 0, y: -window.innerHeight});
-      gsap.to(part, {...animOptions, y: 0}).then(function () {
-        part.children[1].remove();
-        playing = false;
-      })
-    }
-
-    for (let p in parts) {
-      let part = parts[p];
-      let next = document.createElement('div');
-      next.className = 'section';
-      let img = document.createElement('img');
-      img.src = images[current];
-      next.appendChild(img);
-
-      if ((p - Math.max(0, dir)) % 2) {
-        down(part, next);
-      } else {
-        up(part, next);
-      }
-    }
-  }
-}
-
-window.addEventListener('keydown', function(e) {
-  if (['ArrowDown', 'ArrowRight'].includes(e.key)) {
-    go(1);
-  }
-
-  else if (['ArrowUp', 'ArrowLeft'].includes(e.key)) {
-    go(-1);
-  }
-});
-
-function lerp(start, end, amount) {
-  return (1-amount)*start+amount*end
-}
-
-const cursor = document.createElement('div');
-cursor.className = 'cursor';
-
-const cursorF = document.createElement('div');
-cursorF.className = 'cursor-f';
-let cursorX = 0;
-let cursorY = 0;
-let pageX = 0;
-let pageY = 0;
-let size = 8;
-let sizeF = 36;
-let followSpeed = .16;
-
-document.body.appendChild(cursor);
-document.body.appendChild(cursorF);
-
-if ('ontouchstart' in window) {
-  cursor.style.display = 'none';
-  cursorF.style.display = 'none';
-}
-
-cursor.style.setProperty('--size', size+'px');
-cursorF.style.setProperty('--size', sizeF+'px');
-
-window.addEventListener('mousemove', function(e) {
-  pageX = e.clientX;
-  pageY = e.clientY;
-  cursor.style.left = e.clientX-size/2+'px';
-  cursor.style.top = e.clientY-size/2+'px';
-});
-
-function loop() {
-  cursorX = lerp(cursorX, pageX, followSpeed);
-  cursorY = lerp(cursorY, pageY, followSpeed);
-  cursorF.style.top = cursorY - sizeF/2 + 'px';
-  cursorF.style.left = cursorX - sizeF/2 + 'px';
-  requestAnimationFrame(loop);
-}
-
-loop();
-
-let startY;
-let endY;
-let clicked = false;
-
-function mousedown(e) {
-  gsap.to(cursor, {scale: 4.5});
-  gsap.to(cursorF, {scale: .4});
-
-  clicked = true;
-  startY = e.clientY || e.touches[0].clientY || e.targetTouches[0].clientY;
-}
-function mouseup(e) {
-  gsap.to(cursor, {scale: 1});
-  gsap.to(cursorF, {scale: 1});
-
-  endY = e.clientY || endY;
-  if (clicked && startY && Math.abs(startY - endY) >= 40) {
-    go(!Math.min(0, startY - endY)?1:-1);
-    clicked = false;
-    startY = null;
-    endY = null;
-  }
-}
-window.addEventListener('mousedown', mousedown, false);
-window.addEventListener('touchstart', mousedown, false);
-window.addEventListener('touchmove', function(e) {
-  if (clicked) {
-    endY = e.touches[0].clientY || e.targetTouches[0].clientY;
-  }
-}, false);
-window.addEventListener('touchend', mouseup, false);
-window.addEventListener('mouseup', mouseup, false);
-
-let scrollTimeout;
-function wheel(e) {
-  clearTimeout(scrollTimeout);
-  setTimeout(function() {
-    if (e.deltaY < -40) {
-      go(-1);
-    }
-    else if (e.deltaY >= 40) {
-      go(1);
-    }
-  })
-}
-window.addEventListener('mousewheel', wheel, false);
-window.addEventListener('wheel', wheel, false);
+window.onload = rain(document.body);
+window.onresize = rain;
